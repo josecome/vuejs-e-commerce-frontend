71cd2693c357b170cf0e5c28415e63fd7acf1a6e
Jose Jaime Come <josejaimecome@gmail.com>
2023-05-20 16:30:24 +0200

    Minor changes made

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="45" height="45" /> <span>e-commerce</span>
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/login">Login</RouterLink>/
-        <RouterLink to="/register">Register</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/login">Login</RouterLink>/
+      <RouterLink to="/register">Register</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
