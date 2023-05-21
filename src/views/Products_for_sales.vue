67a669485df35170fe54bd92a2c95d9a3b6312e3
Jose Jaime Come <josejaimecome@gmail.com>
2023-05-21 12:12:16 +0200

    Minor changes made

--- src/views/Products_for_sales.vue
+++ src/views/Products_for_sales.vue
@@ -37,32 +37,35 @@
 </style>
 <template>
   <div class="album py-5 bg-light">
-   <div v-for="product_item in list_of_products" :key="id" class="col">
-            <div class="card shadow-sm">
-            <text x="80%" y="80%" fill="#eceeef" dy=".3em">
-            <img :src="'http://127.0.0.1:8000/storage/images/products/' + product_item.image_link"
-                :title="product_item.product"
-                style="width: 100%; height: 100%;"
-            />
-            </text>
-            <div class="card-body" style='background-color: #D4E6F1;'>
-              <p class="card-text">{{ product_item.description }}</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button"  @click="checkProduct($event, product_item.id, product_item.product)"
-                        class="btn btn-sm btn-outline-secondary"
-                        style="color: white;"
-                        :class="product_status[product_item.id] === 1 ? 'addToChrt' : 'rmvToChrt'"
-                    >
-                        {{ product_status[product_item.id] !== null && product_status[product_item.id] === 1 ? 'Remove from Cart' : 'Add to Cart' }}
-                   </button>
-                  <label class="btn btn-sm btn-outline-secondary">{{ '$' + product_item.price  }}</label>
+    <div class="container">
+        <div id="app" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div v-for="product_item in list_of_products" :key="id" class="col">
+                <div class="card shadow-sm">
+                <text x="80%" y="80%" fill="#eceeef" dy=".3em">
+                   <img :src="'http://127.0.0.1:8000/storage/images/products/' + product_item.image_link"
+                   :title="product_item.product"
+                   style="width: 100%; height: 100%;"
+                />
+                </text>
+                <div class="card-body" style='background-color: #D4E6F1;'>
+                    <p class="card-text">{{ product_item.description }}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button"  @click="checkProduct($event, product_item.id, product_item.product)"
+                                class="btn btn-sm btn-outline-secondary"
+                                style="color: white;"
+                                :class="product_status[product_item.id] === 1 ? 'addToChrt' : 'rmvToChrt'"
+                            >
+                               {{ product_status[product_item.id] !== null && product_status[product_item.id] === 1 ? 'Remove from Cart' : 'Add to Cart' }}
+                            </button>
+                            <label class="btn btn-sm btn-outline-secondary">{{ '$' + product_item.price  }}</label>
+                        </div>
+                    <div v-if="product_status[product_item.id] === 1" class="bi bi-cart-check Icn" style="color: #239B56;"></div>
                 </div>
-                <div v-if="product_status[product_item.id] === 1" class="bi bi-cart-check Icn" style="color: #239B56;"></div>
-              </div>
             </div>
             </div>
         </div>   
   </div>
-
+</div>   
+  </div>
 </template>
