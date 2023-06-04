e12c62e8f4e655df39cfa57718345be45147914a
Jose Jaime Come <josejaimecome@gmail.com>
2023-06-04 15:56:05 +0200

    Model and methods to display products in cart added

--- src/views/Products_for_sales.vue
+++ src/views/Products_for_sales.vue
@@ -7,6 +7,8 @@
   const localToken = ref('no_token')
   const list_of_products = ref([])
   const product_status = ref([])
+  const count = ref(0)
+
   let product_type = route.params.category
   const link = ref(`http://127.0.0.1:8000/api/products_for_sale/${ product_type }`)
   console.log(link.value)
@@ -28,6 +30,13 @@
   const setDataInForm = () => {
     console.log('test')
   }
+  const checkProduct = (e, id, b) => {
+            var chk = e.target.textContent;
+            product_status[id] = 1;
+            chk === "Add to Cart" ? count.value++ : count.value--;
+            chk === "Add to Cart" ? Product_in_cart(id, "p") : Product_in_cart(id, "r") //p -> put in Cart and r -> remove from cart
+            chk === "Add to Cart" ? product_status[id] = 1 : product_status[id] = 0;
+  }
   onMounted(getData);
 
 </script>
@@ -39,6 +48,15 @@
   <div class="album py-5 bg-light">
     <div class="container">
         <div id="app" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div style="height: 60px; width: 100%; background-color: #EAEDED;">
+            <span style="float: left; font-size: 28px; padding: 8px;"><strong>Available Products</strong></span>
+            <a href="#" class="notification" style="float: right; padding: 8px;"
+            data-toggle="modal" data-target="#myModal"
+            >
+                <span class="bi bi-cart-check Icn"></span>
+                <span class="badge">{{ count }}</span>
+            </a>
+        </div>
             <div v-for="product_item in list_of_products" :key="id" class="col">
                 <div class="card shadow-sm">
                 <text x="80%" y="80%" fill="#eceeef" dy=".3em">
@@ -68,4 +86,74 @@
   </div>
 </div>   
   </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content" style="width: 100%;">
+        <div class="modal-header" style="width: 100%;">
+          <h4 class="modal-title" style="float: left;"><strong>Products in Cart</strong></h4>
+          <button type="button" class="close" data-dismiss="modal" style="float: right;">&times;</button>
+        </div>
+        <div id="prodInCart" class="modal-body">
+          <table>
+                <thead>
+                    <tr>
+                        <th>Product</th><th>Price</th><th>Qnty</th><th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(product_item_in_cart, index) in Products_in_Cart"
+                        :key="id"
+                        class="col"
+                        style="border-bottom: 2px solid #BFC9CA; padding: 6px;">
+                        <td>
+                            {{ product_item_in_cart.product }}
+                        </td>
+                        <td>
+                            {{ format_to_money_style(product_item_in_cart.price) }}
+                        </td>
+                        <td>
+                            <input type='number'
+                                style='width: 60px'
+                                :disabled="purchase_status !== 'Order'"
+                                v-model = "Qnty_of_Products_in_Cart[index]"
+                                v-on:change="ChangeProductQnty(product_item_in_cart.id, index)"
+                            />
+                        </td>
+                        <td>
+                            <i @click="Product_in_cart(product_item_in_cart.id, 'r')" class="bi bi-x-octagon-fill" style="float: right; padding-left: 6px; color: #E74C3C;"></i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Total</strong>
+                        </td>
+                        <td>
+                        <strong>{{ total_price_to_pay }}</strong>
+                        </td>
+                        <td>
+
+                        </td>
+                    </tr>
+                </tbody>
+          </table>
+        </div>
+        <div class="modal-footer">
+           <form action="/confirm_payment" method="POST" v-show="purchase_status === 'Purchase'">
+               <input type="hidden" name="ids" v-model="Ids_of_Products_in_Cart" />
+               <input type="hidden" name="totalprice" value="{{ total_price_to_pay }}" />
+               <button type="submit" class="btn btn-danger">Purchase</button>
+           </form>
+          <button type="button" :class="purchase_status === 'Order' ? 'btn btn-success' : 'btn btn-danger'"
+              @click="MarkAsOrdered()"  v-show="purchase_status !== 'Purchase'">Order</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+<!--                                                 -->
 </template>
