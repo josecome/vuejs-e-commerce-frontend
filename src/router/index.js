374954ab3f00f8369778fca3480a14c9cf1df3a7
Jose Jaime Come <josejaimecome@gmail.com>
2023-06-08 18:06:12 +0200

    Minor update made

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Logout from '../views/Logout.vue'
+import Home from '../views/Home.vue'
+import Registration from '../views/Registration.vue'
 import Home from '../views/Home.vue'
 import Products_for_sales from '../views/Products_for_sales.vue'
 
