ea6bf9ce10b30957ae8441d8b5772da555e9df3b
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-02 20:34:25 +0200

    Router function to manage user auth status added

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Registration from '../views/Registration.vue'
 import Home from '../views/Home.vue'
 import Products_for_sales from '../views/Products_for_sales.vue'
 import Dashboard from '../views/Dashboard.vue'
+import { storeToRefs } from 'pinia'
+import { useStoreAuth } from '@/stores/auth_store'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,7 +24,10 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/login',
@@ -49,5 +54,21 @@ const router = createRouter({
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(page => page.meta.requiresAuth)) { //to.name !== 'login' && !this.isloggedin
+    const auth_store = useStoreAuth()
+    const {  isLoggedin } = storeToRefs(auth_store)
+    if ( !isLoggedin.value ) { //Is not logged in, go to Login Page
+      next({ 
+        name: 'login' 
+        //path: 'login',
+        //replace: true
+      })
+    } else {
+      next() // go to wherever I'm going
+    }
+  } else {
+    next() // does not require auth, make sure to always call next()!
+  }
+})
 export default router
